1f1485934e8184f7d42d944a5dee8f39303be226
Ann 杏 Kilzer <akilzer@gmail.com>
2024-04-21 19:44:44 +0900

    gh-pages deploy fix

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    base: '/home/', // gh-pages
     resolve: {
         alias: {
             '@': path.resolve(__dirname, 'src')
