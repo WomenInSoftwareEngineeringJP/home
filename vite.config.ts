e80a645d545777ba2d3dbf75b28853095292dc94
Ann 杏 Kilzer <akilzer@gmail.com>
2024-04-21 19:53:42 +0900

    configure gh-pages with 404

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,21 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react()],
-    base: '/home/', // gh-pages
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, 'src')
-        }
-    },
+export default defineConfig(({ command }) => {
+    const config = {
+        plugins: [react()],
+        base: '', // local
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, 'src')
+            }
+        },
+    }
+
+    // gh-pages
+    if (command !== 'serve') {
+        config.base = '/home/'
+    }
+
+    return config
 })
