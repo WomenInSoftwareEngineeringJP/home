df04bbdace8920ede067a6d5423c650624072a6d
Ann 杏 Kilzer <akilzer@gmail.com>
2024-04-21 17:10:15 +0900

    eslint setup

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,31 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
+    root: true,
+    env: { browser: true, es2020: true },
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended-type-checked',
+        'plugin:react-hooks/recommended',
+        'plugin:react/recommended',
+        'plugin:react/jsx-runtime'
     ],
-  },
+    ignorePatterns: ['dist', '.eslintrc.cjs', '*.html'],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        project: ["tsconfig.json", "tsconfig.node.json"]
+    },
+    plugins: [
+        'react-refresh',
+        '@stylistic/ts'
+    ],
+    rules: {
+        'react-refresh/only-export-components': [
+            'warn',
+            { allowConstantExport: true },
+        ],
+        'react/react-in-jsx-scope': 'off',
+        'quotes': ['error', 'single'],
+        '@typescript-eslint/prefer-nullish-coalescing': 'off',
+        '@typescript-eslint/no-unsafe-call': ['off'],
+        '@stylistic/ts/indent': ['error', 4],
+    },
 }
--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -11,33 +11,33 @@ const theme = createTheme({
             dark: purple[700],
             contrastText: '#FFF'
         },
-      secondary: {
-        light: lightBlue[400],
-        main: lightBlue[800],
-        dark: lightBlue[800],
-        contrastText: '#000',
-      },
-      tertiary: {
-        light: teal[400],
-        main: teal[800],
-        dark: teal[900],
-        contrastText: '#fff',
-      },
-      action: {
-        active: purple[100],
-        hover: purple[300],
-        selected: purple[200],
-        disabled: grey[200],
-        disabledBackground: grey[300]
-      },
-      background: {
-        default: '#170D45',
-        paper: deepPurple[900]
-      },
-      divider: lightBlue[300]
+        secondary: {
+            light: lightBlue[400],
+            main: lightBlue[800],
+            dark: lightBlue[800],
+            contrastText: '#000',
+        },
+        tertiary: {
+            light: teal[400],
+            main: teal[800],
+            dark: teal[900],
+            contrastText: '#fff',
+        },
+        action: {
+            active: purple[100],
+            hover: purple[300],
+            selected: purple[200],
+            disabled: grey[200],
+            disabledBackground: grey[300]
+        },
+        background: {
+            default: '#170D45',
+            paper: deepPurple[900]
+        },
+        divider: lightBlue[300]
       
     },
     typography
-  });
+});
 
-  export default theme
+export default theme
