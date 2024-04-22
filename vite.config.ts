c6c35f975e2ddaaa4fe200c8e45449535ca1b47e
Ann Kilzer キルザー杏 <akilzer@gmail.com>
2024-04-23 00:18:02 +0900

    remove /home/ from build path for prod (#12)

--- vite.config.ts
+++ vite.config.ts
@@ -4,21 +4,11 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-    const config = {
-        plugins: [react()],
-        base: '', // local
-        resolve: {
-            alias: {
-                '@': path.resolve(__dirname, 'src')
-            }
-        },
-    }
-
-    // gh-pages
-    if (command !== 'serve') {
-        config.base = '/home/'
-    }
-
-    return config
+export default defineConfig({
+    plugins: [react()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'src')
+        }
+    },
 })
