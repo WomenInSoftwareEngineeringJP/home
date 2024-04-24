1a458df09113e07a3b8a4607c8a398b85ac7fcb3
Ann Kilzer キルザー杏 <akilzer@gmail.com>
2024-04-25 00:25:13 +0900

    add footer social links (#25)

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,14 +1,20 @@
 import { FC } from 'react';
 import BottomNavigation from '@mui/material/BottomNavigation'
 import Paper from '@mui/material/Paper'
-import StarIcon from '@mui/icons-material/Star'
+import InstagramIcon from '@mui/icons-material/Instagram';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import GitHubIcon from '@mui/icons-material/GitHub';
 import FooterIcon from './FooterIcon';
 
 
 const Footer: FC = () => {
     return <Paper sx={{ position: 'static', bottom: 0, left: 0, right: 0 }} elevation={0} aria-label="footer">
         <BottomNavigation sx={{ backgroundColor: '#512da8' }}>
-            <FooterIcon label="Example Icon" icon={<StarIcon />} href="https://example.com" />
+            <FooterIcon label="Instagram" icon={<InstagramIcon />} href="https://www.instagram.com/womeninsoftwarejp/" />
+            <FooterIcon label="LinkedIn" icon={<LinkedInIcon />} href="https://www.linkedin.com/company/womeninsoftwarejp/" />
+            <FooterIcon label="Facebook" icon={<FacebookIcon />} href="https://www.facebook.com/womeninsoftwarejp/" />
+            <FooterIcon label="GitHub" icon={<GitHubIcon />} href="https://github.com/WomenInSoftwareEngineeringJP" />
         </BottomNavigation>
     </Paper>
 }
