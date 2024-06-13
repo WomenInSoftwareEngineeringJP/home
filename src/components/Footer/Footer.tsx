6eca2e366071e133451aea942026d51619264c78
Ann Kilzer キルザー杏 <akilzer@gmail.com>
2024-06-14 02:10:31 +0900

    add connpass link to footer, unit test update (#37)

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,12 +5,14 @@ import InstagramIcon from '@mui/icons-material/Instagram';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import GitHubIcon from '@mui/icons-material/GitHub';
+import EventIcon from '@mui/icons-material/Event';
 import FooterIcon from './FooterIcon';
 
 
 const Footer: FC = () => {
     return <Paper sx={{ position: 'static', bottom: 0, left: 0, right: 0 }} elevation={0} aria-label="footer">
         <BottomNavigation sx={{ backgroundColor: '#512da8' }}>
+            <FooterIcon label="Events" icon={<EventIcon />} href="https://womeninsoftware-japan.connpass.com/" />
             <FooterIcon label="Instagram" icon={<InstagramIcon />} href="https://www.instagram.com/womeninsoftwarejp/" />
             <FooterIcon label="LinkedIn" icon={<LinkedInIcon />} href="https://www.linkedin.com/company/womeninsoftwarejp/" />
             <FooterIcon label="Facebook" icon={<FacebookIcon />} href="https://www.facebook.com/womeninsoftwarejp/" />
