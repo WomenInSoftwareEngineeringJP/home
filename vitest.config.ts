b860250024b9561db2415504a8d78a8a933b001f
Krizza Bullecer <krizzabullecer@gmail.com>
2024-07-06 20:52:30 +0900

    fix vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,34 +1,30 @@
-/// <reference types="vitest" />
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig, mergeConfig } from 'vite';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react()],
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, 'src')
-        }
-    },
-    test: {
-        coverage: {
-            provider: 'istanbul',
-            enabled: true,
-            reporter: ['text', 'json', 'html'],
-            extension: ['.ts', '.tsx'],
-            all: true,
-            include: ['src/**'],
-            exclude: ['src/tests/**', 'src/main.tsx'],
-            watermarks: {
-                statements: [60, 80],
-                functions: [60, 80],
-                branches: [60, 80],
-                lines: [60, 80]
-            }
+import viteConfig from './vite.config';
+
+export default mergeConfig(
+    viteConfig,
+    defineConfig({
+        test: {
+            globals: true,
+            coverage: {
+                provider: 'istanbul',
+                enabled: true,
+                reporter: ['text', 'json', 'html'],
+                extension: ['.ts', '.tsx'],
+                all: true,
+                include: ['src/**'],
+                exclude: ['src/tests/**', 'src/main.tsx'],
+                watermarks: {
+                    statements: [60, 80],
+                    functions: [60, 80],
+                    branches: [60, 80],
+                    lines: [60, 80],
+                },
+            },
+            environment: 'jsdom',
+            setupFiles: ['./src/tests/setup.ts'],
+            exclude: ['node_modules/**', 'e2e/**', 'dist/**'],
         },
-        environment: 'jsdom',
-        setupFiles: ['./src/tests/setup.ts'],
-        exclude: ['node_modules/**', 'e2e/**', 'dist/**']
-    }
-})
+    })
+);
