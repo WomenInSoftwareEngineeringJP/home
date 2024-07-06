26a5454e00d004c9586392403facb24b1f54c975
Krizza Bullecer <krizzabullecer@gmail.com>
2024-07-06 20:55:24 +0900

    locale toggle test

--- src/components/LocaleToggle/__test__/LocaleToggle.test.tsx
+++ src/components/LocaleToggle/__test__/LocaleToggle.test.tsx
@@ -1,27 +1,66 @@
-import { describe, expect, it } from 'vitest'
-import { render } from '@/tests/customRender'
+import { useTranslation } from 'react-i18next';
+import { screen } from '@testing-library/react';
+import { Mock } from 'vitest';
+
+import { render } from '@/tests/customRender';
 import LocaleToggle from '../LocaleToggle';
-import userEvent from '@testing-library/user-event'
-import { screen } from '@testing-library/react'
+
+vi.mock('react-i18next', async (importOriginal) => {
+    const actual = await importOriginal<typeof import('react-i18next')>();
+    return {
+        ...actual,
+        useTranslation: vi.fn(),
+    };
+});
+
+const useTranslationSpy = useTranslation as Mock;
+
+beforeEach(() => {
+    vi.clearAllMocks();
+
+    useTranslationSpy.mockReturnValue({
+        i18n: { changeLanguage: vi.fn(() => new Promise(() => {})) },
+    });
+});
 
 describe('LocaleToggle', () => {
     it('renders correctly', async () => {
-        render(<LocaleToggle />)
+        render(<LocaleToggle />);
 
         const english = await screen.findByText('English');
         const japanese = await screen.findByText('日本語');
         expect(english).toBeInTheDocument();
         expect(japanese).toBeInTheDocument();
     });
-})
 
-it('changes locale when toggling', async () => {
-    render(<LocaleToggle />);
-    const japaneseButton = screen.getByText('日本語');
+    it('changes locale when toggling', async () => {
+        const { user } = render(<LocaleToggle />);
+        const japaneseButton = screen.getByRole('button', { name: '日本語' });
 
-    const user = userEvent.setup()
-    await user.click(japaneseButton)
-    expect(localStorage.getItem('locale')).toBe('ja');
+        await user.click(japaneseButton);
+        expect(localStorage.getItem('locale')).toBe('ja');
 
-    localStorage.removeItem('locale');
+        localStorage.removeItem('locale');
+    });
+
+    it('handles failed changeLanguage error correctly', async () => {
+        // Mock changeLanguage function to throw an error
+        useTranslationSpy.mockReturnValue({
+            i18n: { changeLanguage: vi.fn().mockRejectedValue(new Error()) },
+        });
+
+        const consoleErrorSpy = vi
+            .spyOn(console, 'error')
+            .mockImplementation(() => {});
+
+        const { user } = render(<LocaleToggle />);
+        const japaneseButton = screen.getByRole('button', { name: '日本語' });
+
+        await user.click(japaneseButton);
+        // `console.error` should be called with an error
+        expect(consoleErrorSpy).toHaveBeenCalledWith(expect.any(Error));
+
+        consoleErrorSpy.mockRestore();
+        useTranslationSpy.mockRestore();
+    });
 });
