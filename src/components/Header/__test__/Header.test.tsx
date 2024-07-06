a723dda7333e43f0d41db276dc3a6d20ab041f2e
Krizza Bullecer <krizzabullecer@gmail.com>
2024-07-06 20:54:39 +0900

    header test

--- src/components/Header/__test__/Header.test.tsx
+++ src/components/Header/__test__/Header.test.tsx
@@ -1,14 +1,26 @@
-import { describe, expect, it } from 'vitest'
-import { render } from '@/tests/customRender'
-import { screen } from '@testing-library/react'
-import Header from '../Header'
+import { describe, expect, it, Mock } from 'vitest';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { screen } from '@testing-library/react';
+import { render } from '@/tests/customRender';
+import Header from '../Header';
+
+vi.mock('@mui/material/useMediaQuery');
 
 describe('Header', () => {
     it('should display the Header in desktop mode on wide screens', async () => {
-        render(<Header />)
-        const title = await screen.findByText('WiSE Japan')
-        expect(title).toBeVisible()
-        const toolbar = await screen.findByLabelText('desktop-toolbar')
-        expect(toolbar).toBeVisible()
-    })
-})
+        render(<Header />);
+        const title = await screen.findByText('WiSE Japan');
+        expect(title).toBeVisible();
+        const toolbar = await screen.findByLabelText('desktop-toolbar');
+        expect(toolbar).toBeVisible();
+    });
+
+    it('should display the Header in mobile mode on smaller screens', async () => {
+        (useMediaQuery as Mock).mockReturnValueOnce(true);
+        render(<Header />);
+        const title = await screen.findByText('WiSE Japan');
+        expect(title).toBeVisible();
+        const toolbar = await screen.findByLabelText('mobile-toolbar');
+        expect(toolbar).toBeVisible();
+    });
+});
