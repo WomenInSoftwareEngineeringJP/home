410783a41036b853348492487851c7129575301b
Maria Tenorio Morales <mariat.avantuxlab@gmail.com>
2024-08-25 13:59:27 +0900

    Job Card responsive fix

--- src/components/JobCard/JobCard.tsx
+++ src/components/JobCard/JobCard.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react'
-import { Stack, Grid, Avatar, Typography, Button, Chip, Card, CardContent } from '@mui/material'
+import { Grid, Avatar, Typography, Button, Chip, Card, CardContent } from '@mui/material'
 import BusinessIcon from '@mui/icons-material/Business'
 import LocationOnIcon from '@mui/icons-material/LocationOn'
 import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet'
@@ -21,7 +21,8 @@ const JobCard: FC<JobCardProps> = ({ job }) => {
             <Grid container spacing={1} alignItems="center">
                 <Grid item xs={12} sm={12} md={2} display="flex" justifyContent="center">
                     <Avatar
-                        sx={{ bgcolor: 'grey', width: 90, height: 90, borderRadius: 2 }}
+                        sx={{ bgcolor: 'white', width: 100, height: 100, borderRadius: 2,  img: {objectFit: 'contain', width: '90%', height: '100%'}
+                        }}
                         alt={`${job.company} logo`}
                         src={job.logoUrl}
                     />
@@ -44,11 +45,11 @@ const JobCard: FC<JobCardProps> = ({ job }) => {
                             <Typography fontSize="body2" sx={{ ml: 1 }}>{job.salary}</Typography>
                         </Grid>
                     </Grid>
-                    <Stack direction='row' spacing={1} mt={2} justifyContent="flex-start">
+                    <Grid direction='row' spacing={1} mt={2} justifyContent="flex-start" flexWrap="wrap">
                         {job.tags.map((tag, index) => (
-                            <Chip key={index} label={tag} sx={{ bgcolor: 'secondary.dark', height: 32, borderRadius: '8px' }} />
+                            <Chip key={index} label={tag} sx={{ bgcolor: 'secondary.dark', height: 32, borderRadius: '8px', mt: { xs: 2, sm: 0 }, ml: { xs: 1, sm: 1 }, flexShrink: 0 }} />
                         ))}
-                    </Stack>
+                    </Grid>
                 </Grid>
                 <Grid item xs={12} sm={12} md={2} paddingRight={2} sx={{ order: { xs: -1, sm: -1, md: 0 }, alignSelf: { sm: 'flex-start' } }} display="flex" alignItems="center" justifyContent="right">
                     <AccessTimeIcon sx={{ marginRight: 2 }} />
