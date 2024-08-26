6710a865e99e6bd8b4b58396b3317bb5aeb0d75d
Ann Kilzer キルザー杏 <akilzer@gmail.com>
2024-08-26 11:56:03 +0900

    Eslint stylistic improvements (#62)

--- src/theme/customColorConfig.ts
+++ src/theme/customColorConfig.ts
@@ -9,7 +9,7 @@ declare module '@mui/material/styles' {
     interface Palette {
         tertiary: Palette['primary'];
     }
-  
+
     interface PaletteOptions {
         tertiary?: PaletteOptions['primary'];
     }
