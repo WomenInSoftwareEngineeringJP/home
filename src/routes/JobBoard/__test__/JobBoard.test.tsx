ce4e880bb0289ad1cdbbefd7ebe39c22201e374d
Ann Kilzer キルザー杏 <akilzer@gmail.com>
2025-04-25 09:50:55 +0900

    add code chrysalis jobs (#104)
    * add code chrysalis jobs
    
    * fix test

--- src/routes/JobBoard/__test__/JobBoard.test.tsx
+++ src/routes/JobBoard/__test__/JobBoard.test.tsx
@@ -2,9 +2,9 @@ import { describe, expect, it } from 'vitest'
 import { render } from '@/tests/customRender'
 import { screen } from '@testing-library/react'
 import JobBoard from '../JobBoard'
+import jobs from '../jobs.json'
 
-
-describe('JobBoard', ()=> {
+describe('JobBoard', () => {
     it('should render the JobBoard page', async () => {
         render(<JobBoard />)
 
@@ -12,7 +12,7 @@ describe('JobBoard', ()=> {
         expect(title).toBeVisible()
 
         const jobCards = await screen.findAllByTestId('job-card')
-        expect(jobCards).toHaveLength(3)
+        expect(jobCards).toHaveLength(jobs.length)
         jobCards.forEach(jobCard => expect(jobCard).toBeVisible())
     })
 })
