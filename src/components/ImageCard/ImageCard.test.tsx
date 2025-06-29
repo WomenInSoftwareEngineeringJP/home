44be7570e181216a1ec1c9831d3b43783cf04168
Krizza Bullecer <krizzabullecer@gmail.com>
2025-06-29 20:39:39 +0900

    image card test

---
+++ src/components/ImageCard/ImageCard.test.tsx
@@ -0,0 +1,14 @@
+import { describe, expect, it } from 'vitest'
+import { screen } from '@testing-library/react'
+import { render } from '@/tests/customRender'
+import ImageCard from './ImageCard'
+
+describe('ImageCard', () => {
+    it('should render ImageCard component correctly', async () => {
+        render(
+            <ImageCard src="https://i.imgur.com/pEI5qWM.jpeg" alt="Cat 🐱" />
+        )
+        const image = await screen.findByRole('img')
+        expect(image).toBeVisible()
+    })
+})
