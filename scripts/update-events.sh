3597a0ab86a5c18edfe7e626e3c84249baf71cfb
Olivier Lechevalier <olivier.lechevalier@gmail.com>
2025-10-26 13:42:43 +0900

    feat: add script to update the events.json using connpass API
    The script requires:
    - jq
    - curl
    - connpass API key set as `CONNPASS_API_KEY`

---
+++ scripts/update-events.sh
@@ -0,0 +1,9 @@
+#!env bash
+set -e
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+cd "$SCRIPT_DIR/.."
+if [ -z "$CONNPASS_API_KEY" ]; then
+  echo "Error: CONNPASS_API_KEY environment variable is not set."
+  exit 1
+fi
+curl -H "X-API-Key: $CONNPASS_API_KEY" "https://connpass.com/api/v2/events/?group_id=6753&order=1&count=5" | jq -f events.jq > src/routes/Event/events.json
