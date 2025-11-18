7ebd824cbc21d2a5c5493c251d14cb0c8c182b58
Ann 杏 Kilzer <akilzer@gmail.com>
2025-11-18 23:45:29 +0900

    sample code change as test for copilot

---
+++ src/components/DemoComponent/DemoComponent.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react'
+import Paper from '@mui/material/Paper'
+
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const API_KEY = 'n0t_a_real_api_key_1234567890'
+
+const DemoComponent: FC = () => {
+    return <Paper sx={{ color: '#549903'}}>Demo Component</Paper>
+}
+
+export default DemoComponent
