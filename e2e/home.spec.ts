f8ed81355cff5d46e76d9e27b11454aedf21e8da
Luane Aquino <luane.aquino.br@gmail.com>
2025-11-23 23:11:03 +0900

    test: add test to check drawer closing after item click on mobile

--- e2e/home.spec.ts
+++ e2e/home.spec.ts
@@ -6,3 +6,23 @@ test('has title', async ({ page }) => {
     // Expect a title "to contain" a substring.
     await expect(page).toHaveTitle(/WiSE/)
 })
+
+test.describe('mobile viewport', () => {
+    test.use({ viewport: { width: 400, height: 800 } })
+
+    test('should close drawer when a list item is selected on mobile', async ({ page }) => {
+        await page.goto('/')
+
+        const hamburger = page.getByLabel('drawer-toggle-button')
+        await hamburger.click()
+        const drawer = page.getByLabel('drawer')
+        const events = drawer.getByText('Events')
+
+        await events.click()
+
+        await expect(events).not.toBeVisible()
+        const eventsContainer = page.getByLabel('events-container')
+        const heading = eventsContainer.getByRole('heading', { name: 'Events', exact: true })
+        await expect(heading).toBeVisible()
+    })
+})
